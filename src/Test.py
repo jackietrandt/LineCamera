4a61c6408de76c52d0e8c727736e6934d2190028
Jackie Tran <jackie.trandt@gmail.com>
2013-05-06 19:47:55 +0800

    Tested Test.py with Kivy working
    All it does is say Hello.
    Kivy setup on PC is done

--- src/Test.py
+++ src/Test.py
@@ -1,20 +1,14 @@
-'''
-Created on 30/04/2013
-
-@author: user
-'''
 import kivy
 kivy.require('1.0.6') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
-
+        return Button(text='Hello World')
 
 if __name__ == '__main__':
     MyApp().run()
